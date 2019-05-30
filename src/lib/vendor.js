29c9a3835ad8a16240b5daf56345d22b85cd3a7d
aligundogdu <ali@vuska.net>
2019-05-30 13:33:29 +0300

    add slick slider.

--- src/lib/vendor.js
+++ src/lib/vendor.js
@@ -1,4 +1,4 @@
 require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.min');
 require('../../node_modules/font-awesome/css/font-awesome.min.css');
-
+require('../../node_modules/slick-carousel/slick/slick');
